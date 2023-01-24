df94881317962caae456b6e218ef9ff28d96b97b
Kritikhanal <kkritima@outlook.com>
2023-01-24 14:54:13 +0545

    commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const App = () =>
+{
+  return (
+    <>
+    <h1>Welcome!</h1>
+    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Vel eveniet perferendis consequuntur voluptatibus iusto reprehenderit necessitatibus error repellat? Eum recusandae ipsum voluptas ea pariatur natus sapiente at quos placeat nobis.
+    Consectetur expedita libero soluta accusantium aspernatur culpa ullam fugiat mollitia voluptatem, harum deleniti animi nisi recusandae perferendis labore dolorum ut molestias consequatur error quaerat aliquid optio iste possimus? Nesciunt, nulla!
+    Esse cumque repellendus illum blanditiis magni quaerat odit nulla exercitationem, optio non, quam odio adipisci placeat est harum quo eius facilis nobis asperiores, ipsa quis? Minima, nulla ipsum! Tempora, exercitationem!</p>
+    </>
+  );
+
+};
+
+export default App;
